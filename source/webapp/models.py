88c344d63c8379d55238e09ec85c9b84f1773d93
big-smoke <arturkrmnlv10@gmail.com>
2021-01-23 16:30:42 +0600

    Создана модель сообщения.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    author: AbstractUser = models.OneToOneField(get_user_model(), related_name='author_message',
+                                                on_delete=models.CASCADE, verbose_name='Отправитель')
+    recipient: AbstractUser = models.OneToOneField(get_user_model(), related_name='recipient_message',
+                                                   on_delete=models.CASCADE, verbose_name='Получатель')
+    text = models.TextField(max_length=2000, verbose_name='Текст сообщения')
+
+    def __str__(self):
+        return self.author
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
